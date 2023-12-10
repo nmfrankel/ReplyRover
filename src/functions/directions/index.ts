172f178248cfecefd26d58f9714facfd5b03980f
Nosson M Frankel <nossonmfrankel@gmail.com>
2023-12-10 02:18:48 -0500

    Working directions demo

--- src/functions/directions/index.ts
+++ src/functions/directions/index.ts
@@ -1,41 +1,49 @@
-const basreURL = `https://maps.googleapis.com/maps/api/directions/json?destination=468%20E%209th%20St,%20Brooklyn,%20NY%2011218&origin=5815%2021st%20Ave,%20Brooklyn,%20NY%2011204&key=${process.env.GOOGLE_MAPS_PLATFORM_API_KEY}`
+const baseURL = 'https://maps.googleapis.com/maps/api/directions/json'
 
 // https://developers.google.com/maps/documentation/directions/get-directions
-// https://developers.google.com/maps/documentation/places/web-service
 
 interface HTMLinstructions {
     routes: {
         legs: {
             steps: {
-                html_instructions: string;
-            }[];
+				distance: {
+					text: string;
+					value: number;
+				};
+				duration: {
+					text: string;
+					value: number;
+				};
+				end_location: {
+					lat: number;
+					lng: number;
+				};
+				html_instructions: string;
+				travel_mode: string;
+			}[]
         }[];
     }[];
 }
 
-export const fetchDirections = (msg: string) => {
+const styleHtmlToText = (instruction: string) => {
+	return instruction.replace(/<\/?b>/g, '**').replace('<wbr/>', '').replace(/<[\w\s]+.*?>/g, '\n- ').replace(/<\/[\w\s]+.*?>/g, '').replace('**/**', '/')
+}
 
-	const response: HTMLinstructions = {
-		routes: [
-			{
-				legs: [
-					{
-						steps: [
-							{
-								html_instructions: ''
-							}
-						]
-					}
-				]
-			}
-		]
-	}
+export const fetchDirections = async (msg: string) => {
+	// const modes = ['driving', 'walking', 'bicycling', 'transit']
+	const mode = 'driving'
+	const origin = '1900 Ave M 11230'
+	const destination = '326%20E%209th%20St,%20Brooklyn,%20NY%2011218'
+	const endpoint = `${baseURL}?origin=${origin}&destination=${destination}&key=${process.env.GOOGLE_MAPS_PLATFORM_API_KEY}`
 
-	const steps = response.routes?.[0].legs?.[0].steps
+	const res = await fetch(endpoint)
+	const options = await res.json()
+	const steps = options.routes?.[0].legs?.[0].steps
 
+	let formattedDirections = ''
 	for (const step of steps) {
-		console.log(step.html_instructions)
+		formattedDirections += styleHtmlToText(step.html_instructions) + '\n'
 	}
 
-	return ''
+	return formattedDirections.slice(0, -1)
 }
