de7efcaa604fce9ae0b5e62ea1913160deb5240a
Nosson M Frankel <65728745+nmfrankel@users.noreply.github.com>
2023-12-09 23:14:02 -0500

    API response layout

--- src/functions/directions/index.ts
+++ src/functions/directions/index.ts
@@ -3,8 +3,39 @@ const basreURL = `https://maps.googleapis.com/maps/api/directions/json?destinati
 // https://developers.google.com/maps/documentation/directions/get-directions
 // https://developers.google.com/maps/documentation/places/web-service
 
+interface HTMLinstructions {
+    routes: {
+        legs: {
+            steps: {
+                html_instructions: string;
+            }[];
+        }[];
+    }[];
+}
+
 export const fetchDirections = (msg: string) => {
 
+	const response: HTMLinstructions = {
+		routes: [
+			{
+				legs: [
+					{
+						steps: [
+							{
+								html_instructions: ''
+							}
+						]
+					}
+				]
+			}
+		]
+	}
+
+	const steps = response.routes?.[0].legs?.[0].steps
+
+	for (const step of steps) {
+		console.log(step.html_instructions)
+	}
 
 	return ''
 }
