4ff6413d4d733a05cab5efd374c8d595e84f2b80
Nosson M Frankel <nossonmfrankel@gmail.com>
2023-12-12 22:09:30 -0500

    Refactored lookup feature

--- src/functions/lookup/index.ts
+++ src/functions/lookup/index.ts
@@ -1,105 +1,110 @@
-interface EntitySearch {
-	places?: {
-		nationalPhoneNumber: string
-		formattedAddress: string
-		regularOpeningHours: {
-			openNow: boolean
-			periods: {
-				open: {
-					day: number
-					hour: number
-					minute: number
-				}
-				close: {
-					day: number
-					hour: number
-					minute: number
-				}
-			}[]
-			weekdayDescriptions: string[]
-		}
-		displayName: {
-			text: string
-			languageCode: string
-		}
-	}[]
+import type { Place, EntitySearch, RegularOpeningHours } from './types'
+
+// https://developers.google.com/maps/documentation/places/web-service/text-search
+
+export const entitySearch = async (msg: string): Promise<string | string[]> => {
+	const searchTerm = msg.replace(/lookup\s*(?:for\s)?/, '')
+
+	if (searchTerm.length < 2) return HELP
+
+	const searchResults = await search(searchTerm)
+
+	if (searchResults.error) {
+		return `An unknown error occured while looking up your search. Please try again later.\n\n${HELP}`
+	} else if (!searchResults.places || !searchResults.places.length) {
+		return `No lookup matches found for "${searchTerm}". Please check your input and try again.\n\n${HELP}`
+	}
+
+	const places = searchResults.places.slice(0, 3).map(formatEntity)
+
+	return places
 }
 
-const search = async (entity: string): Promise<EntitySearch> => {
-	const response = await fetch('https://places.googleapis.com/v1/places:searchText', {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-			'X-Goog-FieldMask':
-				'places.displayName,places.formattedAddress,places.nationalPhoneNumber,places.regularOpeningHours',
-			'X-Goog-Api-Key': process.env.GOOGLE_MAPS_PLATFORM_API_KEY
-		},
-		body: JSON.stringify({
-			textQuery: entity,
-			languageCode: 'en'
-		})
-	})
-	return response.json()
-}
-
-const errorMsg =
+const HELP =
 	'Start the message with "lookup" followed by the [company name], [city, state or zipcode].\nI.e. Lookup Beth Medrash Govoha, 08701'
 
-export const entitySearch = async (msg: string) => {
-	const entity = msg.replace(/lookup\s?/, '')
-
-	if (entity.length < 2) return errorMsg
-
-	const entities = await search(entity)
-
-	if (!entities.places) return 'No lookup matches found.\n' + errorMsg
-
-	const places = entities.places.map((place, i) => {
-		if (i >= 3) return
-
-		const cleanedAddress = place?.formattedAddress.replace(', ', '\n')
-
-		let formattedEntities = `${place.displayName.text}\n${cleanedAddress}\n${place.nationalPhoneNumber}`
-
-		// If hours are posted and not open 24/7
-		if (place.regularOpeningHours?.periods.length === 1) {
-			formattedEntities += '\n\nHours\nOpen 24 hours daily, 7 days a week'
-		} else if (place.regularOpeningHours?.periods.length > 1) {
-			formattedEntities += '\n\nHours'
-
-			// Reorder weekdays to start with Sunday
-			let weekdays = [place.regularOpeningHours.weekdayDescriptions.pop()]
-			weekdays = [...weekdays, ...place.regularOpeningHours.weekdayDescriptions]
-
-			// shorten day name from Sunday -> Sun
-			weekdays = weekdays.map((weekday: string) => weekday.replace(/^(\w{3}).*day:(.*)/, '$1:$2'))
-
-			// Combine days with the same hours
-			const mergedDays = weekdays.reduce((result, day) => {
-				const [currentDay, schedule] = day.split(': ')
-				const lastEntry = result[result.length - 1]
-
-				if (lastEntry && lastEntry.schedule === schedule) {
-					lastEntry.endDay = currentDay
-				} else {
-					result.push({ startDay: currentDay, endDay: currentDay, schedule })
-				}
-
-				return result
-			}, [])
-
-			formattedEntities += mergedDays
-				.map(({ startDay, endDay, schedule }) => {
-					if (startDay === endDay) {
-						return `\n${startDay}: ${schedule}`
-					} else {
-						return `\n${startDay} - ${endDay}: ${schedule}`
-					}
-				})
-				.join('')
+const search = async (searchTerm: string): Promise<EntitySearch> => {
+	try {
+		const response = await fetch('https://places.googleapis.com/v1/places:searchText', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				'X-Goog-FieldMask':
+					'places.displayName,places.formattedAddress,places.nationalPhoneNumber,places.regularOpeningHours',
+				'X-Goog-Api-Key': process.env.GOOGLE_MAPS_PLATFORM_API_KEY
+			},
+			body: JSON.stringify({
+				textQuery: searchTerm,
+				languageCode: 'en'
+			})
+		})
+		return response.json()
+	} catch (err) {
+		// tslint:disable-next-line:no-console
+		console.error('[RETRIEVAL_ERROR]: ', err)
+		return {
+			error: {
+				code: 0,
+				message: err,
+				status: 'RETRIEVAL_ERROR'
+			},
+			places: undefined
 		}
+	}
+}
 
-		return formattedEntities
+const formatEntity = (place: Place): string => {
+	const { displayName, formattedAddress, nationalPhoneNumber, regularOpeningHours } = place
+	const modifiedAddress = formattedAddress.replace(', ', '\n')
+
+	let formattedEntity = `${displayName.text}\n${modifiedAddress}\n${nationalPhoneNumber}`
+	formattedEntity += formatHours(regularOpeningHours)
+
+	return formattedEntity
+}
+
+const formatHours = (hours: RegularOpeningHours): string => {
+	if (!hours || hours.periods.length === 0) return
+	else if (hours.periods.length === 1) return '\n\nHours\nOpen 24 hours daily, 7 days a week'
+
+	const isOpenNow = hours.openNow ? 'open' : 'closed'
+	let formattedHours = `\n\nHours ~ currently ${isOpenNow}\n`
+
+	// Shorten day name from Sunday -> Sun
+	const schedule = hours.weekdayDescriptions.map((day) =>
+		day.replace(/^(\w{3}).*day:(.*)/, '$1:$2')
+	)
+
+	// Move the last element (Sunday hours) to the beginning of the array
+	const sundaySchedule = schedule.pop()
+	schedule.unshift(sundaySchedule)
+
+	formattedHours += groupDaysByHours(schedule).join('\n')
+
+	return formattedHours
+}
+
+// Only works for days with overlap consecutively
+const groupDaysByHours = (schedule: string[]): string[] => {
+	const groupedByHours: { [hours: string]: string[] } = {}
+	const uniqueHours: string[] = []
+
+	schedule.forEach((daySchedule) => {
+		const [day, hours] = daySchedule.split(': ')
+		const daysWithOverlap: string[] = (groupedByHours[hours] || []).concat(day)
+		groupedByHours[hours] = daysWithOverlap
+
+		if (!uniqueHours.includes(hours)) {
+			uniqueHours.push(hours)
+		}
 	})
-	return places.slice(0, 3)
+
+	const formattedGroupedDays = uniqueHours.map((hours: string) => {
+		const days = groupedByHours[hours]
+		return days.length > 1
+			? `${days[0]} - ${days[days.length - 1]}: ${hours}`
+			: `${days[0]}: ${hours}`
+	})
+
+	return formattedGroupedDays
 }
---
+++ src/functions/lookup/types.ts
@@ -0,0 +1,44 @@
+export interface Timeslot {
+	day: number
+	hour: number
+	minute: number
+}
+
+export interface Period {
+	open: Timeslot
+	close: Timeslot
+}
+
+export interface RegularOpeningHours {
+	openNow: boolean
+	periods: Period[]
+	weekdayDescriptions: string[]
+}
+
+export interface Place {
+	displayName: {
+		text: string
+		languageCode: string
+	}
+	formattedAddress: string
+	nationalPhoneNumber: string
+	regularOpeningHours: RegularOpeningHours
+	// Add more properties as needed
+}
+
+interface EntitySearchSuccess {
+	error: undefined
+	places: Place[]
+}
+
+interface EntitySearchError {
+	error: {
+		code: number
+		message: string
+		status: string
+		details?: object[]
+	}
+	places: undefined
+}
+
+export type EntitySearch = EntitySearchSuccess | EntitySearchError
