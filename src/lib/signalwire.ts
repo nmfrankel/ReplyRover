9bb6b928d94faca17fa2651d0bb4f5614266d08e
Nosson M Frankel <nossonmfrankel@gmail.com>
2024-05-27 00:19:14 -0400

    Added logging w/ new DB schema

--- src/lib/signalwire.ts
+++ src/lib/signalwire.ts
@@ -1,16 +1,20 @@
 import { SignalWire } from '@signalwire/realtime-api';
 
-export const client = await SignalWire({
-	project: process.env.SIGNALWIRE_PROJECT_ID,
-	token: process.env.SIGNALWIRE_TOKEN
-});
+export async function signalwire(
+	recipient: string,
+	body: string,
+	sender: string = process.env.SIGNALWIRE_PHONE_NUMBER
+) {
+	const client = await SignalWire({
+		project: process.env.SIGNALWIRE_PROJECT_ID,
+		token: process.env.SIGNALWIRE_TOKEN
+	});
 
-export async function signalwire(endpoint: string, msg: string) {
 	try {
-		const sendResult = await client.messaging.send({
-			from: process.env.SIGNALWIRE_PHONE_NUMBER,
-			to: endpoint,
-			body: msg
+		return await client.messaging.send({
+			from: sender,
+			to: recipient,
+			body
 		});
 	} catch (e) {
 		// tslint:disable-next-line:no-console
--- src/webhooks/signalwire/index.ts
+++ src/webhooks/signalwire/index.ts
@@ -1,32 +1,80 @@
 import express from 'express';
 import { function_calling } from '../../functions/index';
-import { event_logger, que_messages } from '../utils';
+import { signalwire } from '$lib/signalwire';
 
 const router = express.Router();
-const HELP_MSG = `I'm ready to assist you with whatever you would like.`;
+
+router.use(async (req, _, next) => {
+	if (process.env.NODE_ENV !== 'production') {
+		next();
+		return;
+	}
+
+	const { From: endpoint, To: system_endpoint, Body: body } = req.body;
+
+	const isNewUser = await db.user.findFirst({
+		where: {
+			endpoint
+		}
+	});
+	const event = isNewUser ? 'message' : 'new_user';
+
+	const loggedEvent = db.event.create({
+		data: {
+			user: {
+				connect: {
+					endpoint
+				}
+			},
+			action: event,
+			body,
+			system_endpoint
+		}
+	});
+
+	// tslint:disable-next-line:no-console
+	console.log(`[SYSTEM] message from ${endpoint} logged [${loggedEvent.id}]`);
+
+	if (event === 'new_user' && HELP_MSG.length > 4) {
+		const HELP_MSG = `I'm ready to assist you with whatever you would like.`;
+
+		await new Promise((r) => setTimeout(r, 5000));
+		await signalwire(endpoint, HELP_MSG, system_endpoint);
+	} else if (event !== 'message') {
+		next();
+		return;
+	}
+
+	next();
+});
 
 router.get('/error', async (req, res) => {
-	const XML_ERROR = `<?xml version="1.0" encoding="UTF-8"?><Response><Message>Our server seems to have a glitch, try again later.</Message></Response>`;
+	const XML_ERROR = `<?xml version="1.0" encoding="UTF-8"?><Response><Message>Our server seems to have a glitched, try again later.</Message></Response>`;
 
 	res.setHeader('content-type', 'application/xml');
 	res.send(XML_ERROR);
 });
 
 router.post('/', async (req, res) => {
-	const { From, To, Body: body } = req.body;
-
-	const logged = await event_logger('message', From, 'N/A', body);
-	// tslint:disable-next-line:no-console
-	console.log(`[SYSTEM] message from ${From} logged [${logged.id}]`);
+	const { From: endpoint, To: system_endpoint, Body: body } = req.body;
 
 	const [reply, clamp, completed] = await function_calling(body);
 
 	if (process.env.NODE_ENV === 'production') {
-		await que_messages(From, reply, clamp);
+		await db.event.create({
+			data: {
+				userID: 'assistant',
+				action: 'response',
+				body: reply,
+				system_endpoint,
+				completed
+			}
+		});
 	}
 
 	res.setHeader('content-type', 'application/xml');
-	res.send(`<?xml version="1.0" encoding="UTF-8"?><Response></Response>`);
+	res.send(`<?xml version="1.0" encoding="UTF-8"?>
+	<Response><Message>${reply}</Message></Response>`);
 });
 
 export default router;
